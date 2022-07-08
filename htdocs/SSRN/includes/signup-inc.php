0749431a55a3ee59e65e9cb1a4915bf4051fd539
Jefferson Hovey <108751195+Tree52@users.noreply.github.com>
2022-07-08 16:52:12 -0400

    Delete htdocs/SSRN directory

--- htdocs/SSRN/includes/signup-inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-  include_once 'dbh-inc.php';
-
-  $email = $_POST['email'];
-
-  $sql = "INSERT INTO `user` (id, email) VALUES (5, '$email');";
-  mysqli_query($conn, $sql);
-
-  header("Location: ../Create-SignIn.php?signup=success");
